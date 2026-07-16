6da6867c07eb7e0205e90c7d97110f0d7f217228
kiluarr <158548855+kiluarr@users.noreply.github.com>
2026-07-16 22:03:19 +0700

    Upgrade service worker cache version and improve fetch handling

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-const CACHE_NAME = "kasir-ayam-potong-v1";
+const CACHE_NAME = "kasir-ayam-potong-v2";
+
 const ASSETS = [
+  "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
@@ -17,24 +19,41 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put("./index.html", copy);
+          });
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((cached) => {
       return (
         cached ||
-        fetch(event.request)
-          .then((res) => {
-            const resClone = res.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
-            return res;
-          })
-          .catch(() => cached)
+        fetch(event.request).then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, copy);
+          });
+          return response;
+        })
       );
     })
   );
